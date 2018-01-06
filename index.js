7e0f82eec69f4905225150ac2ac5066247260350
Peter Leinonen <peter@leinonen.se>
2018-01-06 17:42:04 +0100

    Refactor

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 // Scrape drug information from erowid.org, and save result as json
 
 const R = require('ramda')
-const fs = require('fs')
-const axios = require('axios')
 const cheerio = require('cheerio')
 const { getPage, getBase, slasher, fixUrl, saveToDisk } = require('./utils')
 
 const matching = R.curry((kind, url) => url.includes(kind))
 const sortByNameCaseInsensitive = R.sortBy(R.compose(R.toLower, R.prop('name')));
 
+const invalidDrug = ({ name }) => name === undefined
+
 const printDrug = drug => console.log(drug)
+
+const stripLineFeeds = R.replace(/\n/g, '')
 
 const extractSubstanceLinks = ({ markup }) => {
   const $ = cheerio.load(markup)
@@ -30,15 +32,16 @@ const extractBasicsUrl = (url, $) => {
 const extractBasics = (basicsUrl) => {
   return getPage(basicsUrl).then(({ markup }) => {
     const $ = cheerio.load(markup)
+    const select = selector => stripLineFeeds($(selector).text())
     return {
       basics: {
-        description: $('.basics-text').text(),
-        dose: $('.basics-dose .basics-text').text(),
-        chemistry: $('.basics-chemistry .basics-text').text(),
-        pharmacology: $('.basics-pharmacology .basics-text').text(),
-        law: $('.basics-law .basics-text').text(),
-        history: $('.basics-history .basics-text').text(),
-        effects: $('.basics-effects .basics-text').text(),
+        description: select('.basics-text'),
+        dose: select('.basics-dose .basics-text'),
+        chemistry: select('.basics-chemistry .basics-text'),
+        pharmacology: select('.basics-pharmacology .basics-text'),
+        law: select('.basics-law .basics-text'),
+        history: select('.basics-history .basics-text'),
+        effects: select('.basics-effects .basics-text'),
       }
     };
   });
@@ -48,11 +51,26 @@ const extractDetails = ({ url, markup }) => {
   const $ = cheerio.load(markup)
   const basicsUrl = extractBasicsUrl(url, $);
 
+  const buildArray = R.pipe(
+    R.split(';'),
+    R.map(R.trim),
+    R.reject(e => e.length === 0)
+  )
+
+  const getName = () => {
+    let name = $('.ts-substance-name').text()
+    if (name === '') {
+      name = $('.ts-substance-name img').attr('alt')
+      console.log('NAME', name)
+    }
+    return name
+  }
+
   const details = {
-    name: $('.ts-substance-name').text(),
-    commonName: $('.sum-common-name').text(),
+    name: getName(),
+    commonNames: buildArray($('.sum-common-name').text()),
     description: $('.sum-description').text(),
-    effects: $('.sum-effects').text(),
+    effects: buildArray($('.sum-effects').text()),
     chemicalName: $('.sum-chem-name').text(),
     image: extractDetailImageUrl(url, $),
     url,
@@ -64,14 +82,17 @@ const extractDetails = ({ url, markup }) => {
 
 const fetchDrugDetails = R.composeP(extractDetails, getPage)
 
+const app = R.pipeP(
+  getPage,
+  extractSubstanceLinks,
+  R.map(fixUrl),
+  R.map(fetchDrugDetails),
+  drugPromises => Promise.all(drugPromises),
+  R.reject(invalidDrug),
+  sortByNameCaseInsensitive,
+  saveToDisk('drugs.json'),
+  R.map(printDrug)
+)
 
-getPage('https://erowid.org/psychoactives/psychoactives.shtml')
-  .then(extractSubstanceLinks)
-  .then(R.filter(matching('chemicals'))) // chemicals, plants, pharma
-  .then(R.map(fixUrl))
-  // Promise.resolve(['https://erowid.org/chemicals/dmt/dmt.shtml'])
-  .then(urls => Promise.all(R.map(fetchDrugDetails)(urls)))
-  .then(sortByNameCaseInsensitive)
-  .then(saveToDisk('drugs.json'))
-  .then(R.map(printDrug))
+app('https://erowid.org/psychoactives/psychoactives.shtml')
   .catch(console.error)
--- utils.js
+++ utils.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 
 const getPage = (url) => axios.get(url).then(({ data }) => ({ url, markup: data }))
 const getBase = pageUrl => pageUrl.slice(0, pageUrl.lastIndexOf('/'))
-const slasher = (base, url) => `${base}${url.startsWith('/') ? '' : '/'}${url}`
+const slasher = (base, url) => `${base}${(url || '').startsWith('/') ? '' : '/'}${url}`
 const fixUrl = (url) => slasher(`https://erowid.org`, url)
 
 const matching = R.curry((kind, url) => url.includes(kind))
